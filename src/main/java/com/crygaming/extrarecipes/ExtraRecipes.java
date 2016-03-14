c94632370edb30857fce00c15126f4addccd9c10
Jakob Roberts <jakob.m.roberts@gmail.com>
2016-03-14 13:11:11 -0700

    Problems...

--- src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
+++ src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
@@ -3,16 +3,22 @@ package com.crygaming.extrarecipes;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.*;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+ * Need to rework, discovered that exp bottles give
+ * a varying quantity of EXP.  Will need to make a
+ * custom EXP bottle that has a set quantity of EXP.
+ */
 
 
 public class ExtraRecipes extends JavaPlugin implements Listener {
+	
+	public static ShapelessRecipe recipeEXPBottle;
 	
 	@Override
 	public void onEnable(){
@@ -28,7 +34,19 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 	@EventHandler
 	public void itemCraft(CraftItemEvent event){
 		Player p = (Player) event.getWhoClicked();
+		Material result = event.getRecipe().getResult().getType();
+		ItemStack[] sourceItems = event.getInventory().getContents();
+		
+		
 		//use this event to subtract specific things from player (such as exp) based on crafting recipe
+		p.sendMessage("YERH CRAFTIN");
+		
+		if(result == Material.EXP_BOTTLE){
+			//possible check for ingredients from sourceItems?
+//			p.
+			p.sendMessage("YOU GOT AN EXP BOTTLE");
+		}
+		
 		
 	}
 	
@@ -43,7 +61,7 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 	/* Recipes Below */
 	/*========================================================*/
 	private void createEXPBottle(){
-		ShapelessRecipe recipeEXPBottle = new ShapelessRecipe(new ItemStack(Material.EXP_BOTTLE));
+		recipeEXPBottle = new ShapelessRecipe(new ItemStack(Material.EXP_BOTTLE));
 		recipeEXPBottle.addIngredient(Material.GLASS_BOTTLE).addIngredient(Material.REDSTONE);
 		this.getServer().addRecipe(recipeEXPBottle);
 		//MUST REMOVE EXP FROM PLAYER WITH CRAFT EVENT!
