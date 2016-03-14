bb6bc939837a08eb572f41e38cda187eb3dad44c
Jakob Roberts <jakob.m.roberts@gmail.com>
2016-03-14 15:37:52 -0700

    Maybe went too deep...
    Need to either find a way to toss a potion like EXP without
    the default listener.  Or revert back to using the default EXP
    bottle potion and just modify the base EXP on the event happen.
    Will then still need to remove a set EXP qty from player on
    craft event.

--- src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
+++ src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
@@ -4,7 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExpBottleEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,6 +17,11 @@ import com.crygaming.extrarecipes.utilities.NewItemBuilder;
  * Need to rework, discovered that exp bottles give
  * a varying quantity of EXP.  Will need to make a
  * custom EXP bottle that has a set quantity of EXP.
+ * 
+ * Could possibly rework that ALL exp bottles give 
+ * the same exp quantity.  Means could revert to
+ * original craft item and have listener/method
+ * to remove exp from player?
  */
 
 
@@ -33,6 +40,18 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 	public void onDisable(){}
 	
 	@EventHandler
+	public void tossEXPBottle(ExpBottleEvent event){
+		//possible? if toss is type of bottle, then change to diff exp give
+		//maybe...
+		
+	}
+	
+	@EventHandler
+    public void expChange(PlayerExpChangeEvent event) {
+		
+	}
+	
+	@EventHandler
 	public void itemCraft(CraftItemEvent event){
 		Player p = (Player) event.getWhoClicked();
 		Material result = event.getRecipe().getResult().getType();
@@ -41,6 +60,7 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 		
 		//use this event to subtract specific things from player (such as exp) based on crafting recipe
 		p.sendMessage("YERH CRAFTIN");
+		p.sendMessage(craftedEXPBottle.getClass().toString());
 		
 		if(result == Material.EXP_BOTTLE){
 			int redstoneCheck = 0;
@@ -95,6 +115,8 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 	//Others to add:
 	/* Sapling to house?
 	 * string to cobweb and vice-versa
+	 * cook bones to make blaze rods?
+	 * slime and redstone to make token, cook token into fire charge
 	 * slabs to blocks
 	 * breakdown tools in anvil to base materials based on durability (different mod maybe?)
 	 */
