d5a113fa754bf1a0cff752b6a61fb4ee6ccc0e3c
Jakob Roberts <jakob.m.roberts@gmail.com>
2016-03-21 13:15:23 -0700

    Forgot to push before OOPS
    Add Elytra Recipe
    Add String to Web Recipe
    Add Web to String Recipe
    Add Wool to String Recipe
    Add further ideas in bottom comments

--- src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
+++ src/main/java/com/crygaming/extrarecipes/ExtraRecipes.java
@@ -8,6 +8,7 @@ import org.bukkit.event.entity.ExpBottleEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,7 +29,7 @@ import com.crygaming.extrarecipes.utilities.NewItemBuilder;
 public class ExtraRecipes extends JavaPlugin implements Listener {
 	
 	public static ShapelessRecipe recipeEXPBottle;
-	public static ItemStack craftedEXPBottle;
+	public static ItemStack craftedEXPBottle;	
 	
 	@Override
 	public void onEnable(){
@@ -37,19 +38,21 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 	}
 	
 	@Override
-	public void onDisable(){}
-	
-	@EventHandler
-	public void tossEXPBottle(ExpBottleEvent event){
-		//possible? if toss is type of bottle, then change to diff exp give
-		//maybe...
-		
+	public void onDisable(){
+		this.getServer().clearRecipes();
 	}
 	
-	@EventHandler
-    public void expChange(PlayerExpChangeEvent event) {
-		
-	}
+//	@EventHandler
+//	public void tossEXPBottle(ExpBottleEvent event){
+//		//possible? if toss is type of bottle, then change to diff exp give
+//		//maybe...cd 
+//		
+//	}
+	
+//	@EventHandler
+//    public void expChange(PlayerExpChangeEvent event) {
+//		
+//	}
 	
 	@EventHandler
 	public void itemCraft(CraftItemEvent event){
@@ -57,67 +60,90 @@ public class ExtraRecipes extends JavaPlugin implements Listener {
 		Material result = event.getRecipe().getResult().getType();
 		ItemStack[] sourceItems = event.getInventory().getContents();
 		
+		this.getLogger().info(p.toString() + " Crafted " + result.toString());
+		
 		
 		//use this event to subtract specific things from player (such as exp) based on crafting recipe
-		p.sendMessage("YERH CRAFTIN");
-		p.sendMessage(craftedEXPBottle.getClass().toString());
+//		p.sendMessage("YERH CRAFTIN");
+//		p.sendMessage(craftedEXPBottle.getClass().toString());
 		
-		if(result == Material.EXP_BOTTLE){
-			int redstoneCheck = 0;
-			int glassBottleCheck = 0;
-			int resultCheck = 0;
-			int other = 0;
-			for(int i=0;i<sourceItems.length;i++){
-				if(sourceItems[i].getType() == Material.REDSTONE){
-					p.sendMessage("redstone");
-					redstoneCheck++;
-				}else if(sourceItems[i].getType() == Material.GLASS_BOTTLE){
-					p.sendMessage("bottle");
-					glassBottleCheck++;
-				}else if(sourceItems[i].getType() == Material.EXP_BOTTLE){
-					p.sendMessage("result");
-					resultCheck++;
-				}else if(sourceItems[i].getType() != Material.AIR){
-					p.sendMessage(sourceItems[i].getType().toString());
-					other++;
-				}
-			}
-			p.sendMessage(redstoneCheck + " " + glassBottleCheck + " "+ other);
-			if(redstoneCheck == 1 && glassBottleCheck == 1 && resultCheck == 1 && other == 0){
-				//remove exp from player
-				p.sendMessage("YOU GOT AN EXP BOTTLE");
-			}else{
-				p.sendMessage("Canceled craft!");
-				event.setCancelled(true);
-			}			
-		}
+//		if(result == Material.EXP_BOTTLE){}
+//		if(result == Material.ELYTRA){}
+		
 	}
 	
 	/*
 	 * List of Added Recipes
+	 * Comment out if want to remove recipe temporarily
 	 */
 	private void addRecipes(){
-		createEXPBottle();
+		
+		//new recipes
+//		createEXPBottle();
+		createElytra();			//crafts Elytra wings
+		wool2String();			//changes wool to string
+		
+		//conversions
+		swapStringWeb();		//changes web to string, and string to web
+		
 	}
 	
 	/*========================================================*/
 	/* Recipes Below */
 	/*========================================================*/
-	private void createEXPBottle(){
-		craftedEXPBottle = NewItemBuilder.begin().setName("Crafted EXP Bottle").setType(Material.EXP_BOTTLE).create();
-		
-		recipeEXPBottle = new ShapelessRecipe(craftedEXPBottle);
-		recipeEXPBottle.addIngredient(Material.GLASS_BOTTLE).addIngredient(Material.REDSTONE);
-		this.getServer().addRecipe(recipeEXPBottle);
-		//MUST REMOVE EXP FROM PLAYER WITH CRAFT EVENT!
+	//	private void createEXPBottle(){
+	//		craftedEXPBottle = NewItemBuilder.begin().setName("Crafted EXP Bottle").setType(Material.EXP_BOTTLE).create();
+	//		recipeEXPBottle = new ShapelessRecipe(craftedEXPBottle);
+	//		recipeEXPBottle.addIngredient(Material.GLASS_BOTTLE).addIngredient(Material.REDSTONE);
+	//		this.getServer().addRecipe(recipeEXPBottle);
+	//		//MUST REMOVE EXP FROM PLAYER WITH CRAFT EVENT!
+	//		//must make recipe/potion a consumable!
+	//	}
+	
+	private void createElytra(){
+		ShapedRecipe recipeElytra;
+		ItemStack craftedElytra;
+		craftedElytra = new ItemStack(Material.ELYTRA);
+		recipeElytra = new ShapedRecipe(craftedElytra);
+		recipeElytra.shape("SLS","FTF","FTF");
+		recipeElytra.setIngredient('L', Material.LEATHER);
+		recipeElytra.setIngredient('S', Material.STICK);
+		recipeElytra.setIngredient('F', Material.FEATHER);
+		recipeElytra.setIngredient('T', Material.STRING);
+		this.getServer().addRecipe(recipeElytra);
 	}
+	
+	private void swapStringWeb(){
+		ShapelessRecipe web2string;
+		web2string = new ShapelessRecipe(new ItemStack(Material.STRING, 9));
+		web2string.addIngredient(Material.WEB);
+		this.getServer().addRecipe(web2string);
+		
+		ShapedRecipe string2web;
+		string2web = new ShapedRecipe(new ItemStack(Material.WEB));
+		string2web.shape("SSS","SSS","SSS");
+		string2web.setIngredient('S', Material.STRING);
+		this.getServer().addRecipe(string2web);
+	}
+	
+	private void wool2String(){
+		ShapelessRecipe wool2string;
+		wool2string = new ShapelessRecipe(new ItemStack(Material.STRING, 4));
+		wool2string.addIngredient(Material.WOOL);
+		this.getServer().addRecipe(wool2string);
+	}
+	
+	
 	
 	//Others to add:
 	/* Sapling to house?
-	 * string to cobweb and vice-versa
+	 * string to cobweb and vice-versa DONE
 	 * cook bones to make blaze rods?
-	 * slime and redstone to make token, cook token into fire charge
-	 * slabs to blocks
+	 * slime and redstone to make token, cook token into fire charge??
+	 * slabs to blocks???
 	 * breakdown tools in anvil to base materials based on durability (different mod maybe?)
+	 * compressed cobblestone -> bedrock
+	 * bedrock x9 -> diamond
+	 * compressed cobblestone + redstone -> obsidian
 	 */
 }
